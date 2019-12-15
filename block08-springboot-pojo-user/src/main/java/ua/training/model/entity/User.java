9af5c1c5822b0315b97993676cf4d92258375d2d
smussergey <smussergey@gmail.com>
2019-12-16 00:13:15 +0200

    Class User was refactored: lombok was added

--- block08-springboot-pojo-user/src/main/java/ua/training/model/entity/User.java
+++ block08-springboot-pojo-user/src/main/java/ua/training/model/entity/User.java
@@ -2,51 +2,13 @@ package ua.training.model.entity;
 
 import lombok.*;
 
-
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class User {
     private String name;
     private String email;
     private String password;
-
-    public User() {
-    }
-
-    public User(String name, String email, String password) {
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public String toString() {
-        return "User{" +
-                "name='" + name + '\'' +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
 }
