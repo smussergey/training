41d702148f35811c389fe22e2888a0011cd6a0a5
smussergey <smussergey@gmail.com>
2020-01-16 17:12:46 +0200

    method logic was simplified and improved module:block08-task01-count-repeated-elements

--- block08-task01-count-repeated-elements/src/main/java/ua/training/CountRepeatedElementsApp.java
+++ block08-task01-count-repeated-elements/src/main/java/ua/training/CountRepeatedElementsApp.java
@@ -10,30 +10,13 @@ import static java.util.stream.Collectors.groupingBy;
 public class CountRepeatedElementsApp {
     public static void main(String[] args) {
         int[] el = {4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7};
-        List<Integer> elements = new ArrayList<>();
-        elements.add(4);
-        elements.add(5);
-        elements.add(-6);
-        elements.add(4);
-        elements.add(5);
-        elements.add(3);
-        elements.add(4);
-        elements.add(2);
-        elements.add(4);
-        elements.add(5);
-        elements.add(7);
+        List<Integer> elements = Arrays.asList(4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7);
 
-        Map<Integer, Long> repeatedElements = groupByValueAndCountOccurrence(elements);
-
-        repeatedElements
+        elements.stream().
+                collect(groupingBy(Function.identity(), counting()))
                 .entrySet()
                 .stream()
                 .sorted(comparingByKey())
                 .forEach(entry -> System.out.println(entry.getKey() + "-" + entry.getValue()));
-    }
-
-    private static Map<Integer, Long> groupByValueAndCountOccurrence(List<Integer> el) {
-        return el.stream()
-                .collect(groupingBy(Function.identity(), counting()));
     }
 }
