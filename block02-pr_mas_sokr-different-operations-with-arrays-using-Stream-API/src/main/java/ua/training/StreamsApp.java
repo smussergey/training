7782f8885d9e7a5287bfee80045e6c8fea952ef4
smussergey <smussergey@gmail.com>
2020-01-20 16:46:13 +0200

    3 completed exercises were added to module: block02-pr_mas_sokr-different-operations-with-arrays

--- block02-pr_mas_sokr-different-operations-with-arrays-using-Stream-API/src/main/java/ua/training/StreamsApp.java
+++ block02-pr_mas_sokr-different-operations-with-arrays-using-Stream-API/src/main/java/ua/training/StreamsApp.java
@@ -5,18 +5,19 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.util.Map.Entry.comparingByKey;
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.*;
 
-public class App {
+public class StreamsApp {
     private static int[] generateTestData() {
-        int[] elements = {4, 5, -6, 4, 5, 3, 4, -7, 1, 2, 4, 5, 7, 15};
+        int[] elements = {4, 5, -6, 4, 5, 0, 3, 4, -7, 1, 2, 4, 5, 7, 0, 15};
         //return Arrays.copyOf(elements,elements.length);
         return elements;
     }
 
     public static void main(String[] args) {
+        multiplyArrayElementsByNumber(generateTestData(), 10);
+        getCountOfElementsGreaterThanZero(generateTestData());
+        getCountOfElementsEquelsToZero(generateTestData());
         getCountOfElementsEquelToTarget(generateTestData(), 5);
         getAverageValueOfAllElement(generateTestData());
         getMinElementValueAndIndex(generateTestData());
@@ -26,9 +27,44 @@ public class App {
 
     }
 
+    private static void multiplyArrayElementsByNumber(int[] elements, int multiplier) {
+        System.out.println("     8. multiplyArrayElementsByNumber: ");
+        printlArray(elements);
+        Arrays.stream(elements)
+                .mapToLong(el -> el * multiplier)
+                .forEach(System.out::print);
+        System.out.println();
+    }
+
+    //7
+    public static void getCountOfElementsGreaterThanZero(int[] elements) {
+        System.out.println("     7. getCountOfElementsGreaterThanZero: ");
+        printlArray(elements);
+        long result;
+        result = Arrays.stream(elements)
+                .filter(el -> el > 0)
+                .count();
+        System.out.println("Elements greater than 0  appears : " + result + " times");
+    }
+
+    //6
+    public static void getCountOfElementsEquelsToZero(int[] elements) {
+        System.out.println("     6. getCountOfElementsEquelsToZero: " + 0);
+        printlArray(elements);
+        Arrays.stream(elements)
+                .mapToObj(Long::valueOf)
+                .collect(groupingBy(Function.identity(), counting()))
+                .entrySet()
+                .stream()
+                .filter(el -> el.getKey() == 0)
+                .findAny()
+                .ifPresent(entry -> System.out.println("Element: " + entry.getKey() + " appears : " + entry.getValue() + " times"));
+    }
+
+
     //5
     public static void getCountOfElementsEquelToTarget(int[] elements, int targetElement) {
-        System.out.println("5. getCountOfElementsEquelToTarget: " + targetElement);
+        System.out.println("     5. getCountOfElementsEquelToTarget: " + targetElement);
         printlArray(elements);
         Arrays.stream(elements)
                 .mapToObj(Long::valueOf)
@@ -42,7 +78,7 @@ public class App {
 
     //4.
     public static void getAverageValueOfAllElement(int[] elements) {
-        System.out.println("4. getAverageValueOfAllElement: " + Arrays.stream(elements)
+        System.out.println("     4. getAverageValueOfAllElement: " + Arrays.stream(elements)
                 .summaryStatistics()
                 .getAverage());
 
@@ -51,7 +87,7 @@ public class App {
 
     //3.
     public static void getMinElementValueAndIndex(int[] elements) {
-        System.out.println("3. getMinElementValueAndIndex:");
+        System.out.println("     3. getMinElementValueAndIndex:");
         printlArray(elements);
 
         IntStream.range(0, elements.length)
@@ -64,7 +100,7 @@ public class App {
 
     //2.
     public static void getMaxElementValueAndIndex(int[] elements) {
-        System.out.println("2. getMaxElementValueAndIndex:");
+        System.out.println("     2. getMaxElementValueAndIndex:");
         printlArray(elements);
 
         IntStream.range(0, elements.length)
@@ -82,7 +118,7 @@ public class App {
 //                    System.out.println(" acam = " + accumulator);
                     return el + accumulator;
                 })
-                .ifPresent(sum -> System.out.println("1. getSumOfArrayElements = " + sum));
+                .ifPresent(sum -> System.out.println("     1. getSumOfArrayElements = " + sum));
 
     }
 
